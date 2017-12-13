9de887b48901a49e3c297eda2c6a189f65a7d7db
SimplyRin <simplyrin.dev@gmail.com>
2017-12-13 17:30:18 +0900

    Add files via upload

--- src/main/java/net/simplyrin/prefixjoinquitmsg/PrefixJoinQuitMsg.java
+++ src/main/java/net/simplyrin/prefixjoinquitmsg/PrefixJoinQuitMsg.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 
 @Mod(modid = PrefixJoinQuitMsg.MODID, version = PrefixJoinQuitMsg.VERSION)
 public class PrefixJoinQuitMsg {
@@ -43,7 +44,11 @@ public class PrefixJoinQuitMsg {
 			return;
 		}
 
-		if(args == null) {
+		try {
+			if(args == null) {
+				return;
+			}
+		} catch (Exception e) {
 			return;
 		}
 
@@ -80,6 +85,44 @@ public class PrefixJoinQuitMsg {
 		}
 	}
 
+	@SubscribeEvent
+	public void onConnect(ClientConnectedToServerEvent event) {
+		Multithreading.runAsync(() -> {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if(!isHypixel) {
+				return;
+			}
+
+			String result = Sk1erMod.rawWithAgent("https://api.simplyrin.net/Forge-Mods/PrefixJoinQuitMsg/" + VERSION);
+
+			if(!result.contains(",") || result == null) {
+				ChatHandler.send(getPrefix() + "&cサーバーへの接続に失敗したか、間違ったバージョンを使用している可能性があります。");
+				return;
+			}
+
+			Boolean update = Boolean.valueOf(result.split(",")[0]);
+			String version = ChatColor.translateAlternateColorCodes('&', result.split(",")[1]);
+			String msg = ChatColor.translateAlternateColorCodes('&', result.split(",")[2]);
+
+			if(!update) {
+				return;
+			}
+
+			ChatHandler.send(getPrefix() + "§b§m-------------------------");
+			ChatHandler.send(getPrefix() + "§b新しいバージョンが使用できます。");
+			ChatHandler.send(getPrefix() + " ");
+			ChatHandler.send(getPrefix() + "§bバージョン: " + version);
+			ChatHandler.send(getPrefix() + "§bメッセージ: " + msg);
+			ChatHandler.send(getPrefix() + " ");
+			ChatHandler.send(getPrefix() + "§b§m-------------------------");
+		});
+	}
+
 	private static void post(String name, TYPE type) {
 		Multithreading.runAsync(() -> {
 			String result = Sk1erMod.rawWithAgent("http://hypixel.chattriggers.com/stats/playerinfo/" + name);
@@ -102,6 +145,10 @@ public class PrefixJoinQuitMsg {
 				ChatHandler.send("§7[§c-§7] §r" + prefix + " " + name + " §eleft.");
 			}
 		});
+	}
+
+	public static String getPrefix() {
+		return "§7[§cPrefixJoinQuitMsg§7] §r";
 	}
 
 	public static enum TYPE {
